e9aa1487e8152c97ba4c2354087f77d0f714d508
jamesLaurino <petersthomas90@gmail.com>
2025-06-13 15:55:11 +0200

    Modification des logo dans register & login

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -81,6 +81,10 @@ const submit = () => {
                     Forgot your password?
                 </Link>
 
+                <Link :href="route('register')" class=" ml-3 inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-50 transition ease-in-out duration-150">
+                    Register
+                </Link>
+
                 <PrimaryButton class="ms-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
                     Log in
                 </PrimaryButton>
