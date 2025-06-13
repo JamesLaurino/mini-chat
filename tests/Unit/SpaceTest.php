f7a6ea29cccfe4ea5deaed9d05311b7e89598434
jamesLaurino <petersthomas90@gmail.com>
2025-06-13 21:34:09 +0200

    Ajout du test de suppression d'un space

--- tests/Unit/SpaceTest.php
+++ tests/Unit/SpaceTest.php
@@ -2,6 +2,7 @@
 
 use App\Models\Space;
 use App\Models\User;
+use App\Services\SpaceService;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 uses(Tests\TestCase::class);
@@ -54,5 +55,35 @@ it('getSpaceByUserId', function () {
     expect($res)->toHaveLength(2);
     expect($res[0]["titre"])->toBe("un titre de test");
     expect($res[1]["titre"])->toBe("un titre de test 2");
+    expect($res[0]['user_id'])->toBe($user->id);;
+});
+
+it('deleteSpaceById', function () {
+
+    // GIVEN
+    $user = User::factory()->create([
+        'name' => 'Alice',
+    ]);
+    $this->actingAs($user);
+
+     Space::factory()->create([
+        "titre" => "un titre de test",
+        "user_id" => $user->id
+    ]);
+
+    $spaceTwo = Space::factory()->create([
+        "titre" => "un titre de test 2",
+        "user_id" => $user->id
+    ]);
+
+    // WHEN
+    $service = app(SpaceService::class);
+    $service->deleteSpaceById($spaceTwo->id);
+
+    $res = Space::all();
+
+    // THEN
+    expect($res)->toHaveLength(1);
+    expect($res[0]["titre"])->toBe("un titre de test");
     expect($res[0]['user_id'])->toBe($user->id);;
 });
